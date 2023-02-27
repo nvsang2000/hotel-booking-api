8d4c8de40b71f355d32fb853733f876a7b338f27
nvsang260720 <nvsang.2670@gmail.com>
2023-02-27 20:44:17 +0700

    change db

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var connect = mysql.createConnection({
   host: "sql9.freemysqlhosting.net",
-  user: "sql9599743",
-  password: "bD5aUI1yDY",
-  database: "sql9599743",
+  user: "sql9601403",
+  password: "8lyetW1BSB",
+  database: "sql9601403",
 });
 
 connect.connect(function (err) {
